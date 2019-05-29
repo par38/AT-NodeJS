aba38f0ddabe956cb2d57d539aae2fbfd800076d
Monica TARABUSI <code.mta@gmail.com>
2019-05-29 11:36:13 +0200

    GET sur home.js, projets.js et details.js + MaJ indexRoutes.js

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express')
 
-const Router = express.Router();
-
+const homeRouter = require('./home')
 const projetsRouter = require('./projets')
 const detailsRouter = require('./details')
 // + continuer routes
 
+const Router = express.Router();
+
+Router.use('/', homeRouter)
 Router.use('/projets', projetsRouter)
 // / où ProjetsRouter est l'alias résultant de l'importation
 Router.use('/details', detailsRouter)
