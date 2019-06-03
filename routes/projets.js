e11d571015384a45abb927139f967755a0305bf1
Monica TARABUSI <code.mta@gmail.com>
2019-06-03 17:50:48 +0200

    routes/home http://localhost:8001/api + routes/projets http://localhost:8001/api/projets

--- routes/projets.js
+++ routes/projets.js
@@ -3,13 +3,14 @@ const connection = require('../config/db')
 
 const Router = express.Router();
 
-// /http://localhost:8001/projets
+// /http://localhost:8001/api/projets
 // * GET FrProjetsCardsAxios et FrProjetsCardsM
 // * main, small picture, city, title + ids (projets + media)
 // / attention : indiquer seulement '/', car s'ajoute à '/projets' de routes.js
 Router.get('/', (req, res) => {
 
-  const projectsList = 'SELECT m.projects_id, m.alt, m.picture_small, p.id, p.city, p.title FROM media AS m JOIN projects AS p ON m.projects_id = p.id WHERE m.main = 1';
+  const projectsList = 'SELECT m.alt, m.picture_small, p.id, p.city, p.title FROM media AS m JOIN projects AS p ON m.projects_id = p.id WHERE m.main = 1';
+  
   connection.query(projectsList, (err, result) => {
     if (err) throw err;
     return res.status(200).send(result);
